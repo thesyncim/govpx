b9c61de70cfe1ac34d7a85ac0c8f04535dba5007
thesyncim <thesyncim@gmail.com>
2026-05-05 18:36:09 +0100

    Add VP8 4x4 intra predictors

---
+++ internal/vp8/dsp/intra4x4.go
@@ -0,0 +1,241 @@
+package dsp
+
+import "github.com/thesyncim/libgopx/internal/vp8/common"
+
+// Ported from libvpx v1.16.0 vpx_dsp/intrapred.c and
+// vp8/common/reconintra4x4.c.
+
+func Intra4x4Predict(dst []byte, dstStride int, mode common.BPredictionMode, above []byte, left []byte, topLeft byte) bool {
+	switch mode {
+	case common.BDCPred:
+		Intra4x4DCPredict(dst, dstStride, above, left)
+	case common.BTMPred:
+		Intra4x4TMPredict(dst, dstStride, above, left, topLeft)
+	case common.BVEPred:
+		Intra4x4VEPredict(dst, dstStride, above, topLeft)
+	case common.BHEPred:
+		Intra4x4HEPredict(dst, dstStride, left, topLeft)
+	case common.BLDPred:
+		Intra4x4LDPredict(dst, dstStride, above)
+	case common.BRDPred:
+		Intra4x4RDPredict(dst, dstStride, above, left, topLeft)
+	case common.BVRPred:
+		Intra4x4VRPredict(dst, dstStride, above, left, topLeft)
+	case common.BVLPred:
+		Intra4x4VLPredict(dst, dstStride, above)
+	case common.BHDPred:
+		Intra4x4HDPredict(dst, dstStride, above, left, topLeft)
+	case common.BHUPred:
+		Intra4x4HUPredict(dst, dstStride, left)
+	default:
+		return false
+	}
+	return true
+}
+
+func Intra4x4DCPredict(dst []byte, dstStride int, above []byte, left []byte) {
+	intraDCPredict(dst, dstStride, above, left, 4, true, true)
+}
+
+func Intra4x4TMPredict(dst []byte, dstStride int, above []byte, left []byte, topLeft byte) {
+	intraTMPredict(dst, dstStride, above, left, topLeft, 4)
+}
+
+func Intra4x4VEPredict(dst []byte, dstStride int, above []byte, topLeft byte) {
+	_ = above[4]
+	_ = dst[3*dstStride+3]
+
+	row := [4]byte{
+		avg3(topLeft, above[0], above[1]),
+		avg3(above[0], above[1], above[2]),
+		avg3(above[1], above[2], above[3]),
+		avg3(above[2], above[3], above[4]),
+	}
+	for y := 0; y < 4; y++ {
+		copy(dst[y*dstStride:y*dstStride+4], row[:])
+	}
+}
+
+func Intra4x4HEPredict(dst []byte, dstStride int, left []byte, topLeft byte) {
+	_ = left[3]
+	_ = dst[3*dstStride+3]
+
+	fillRow4(dst[0*dstStride:], avg3(topLeft, left[0], left[1]))
+	fillRow4(dst[1*dstStride:], avg3(left[0], left[1], left[2]))
+	fillRow4(dst[2*dstStride:], avg3(left[1], left[2], left[3]))
+	fillRow4(dst[3*dstStride:], avg3(left[2], left[3], left[3]))
+}
+
+func Intra4x4LDPredict(dst []byte, dstStride int, above []byte) {
+	_ = above[7]
+	_ = dst[3*dstStride+3]
+
+	a, b, c, d := above[0], above[1], above[2], above[3]
+	e, f, g, h := above[4], above[5], above[6], above[7]
+
+	dst[0*dstStride+0] = avg3(a, b, c)
+	dst[0*dstStride+1] = avg3(b, c, d)
+	dst[1*dstStride+0] = dst[0*dstStride+1]
+	dst[0*dstStride+2] = avg3(c, d, e)
+	dst[1*dstStride+1] = dst[0*dstStride+2]
+	dst[2*dstStride+0] = dst[0*dstStride+2]
+	dst[0*dstStride+3] = avg3(d, e, f)
+	dst[1*dstStride+2] = dst[0*dstStride+3]
+	dst[2*dstStride+1] = dst[0*dstStride+3]
+	dst[3*dstStride+0] = dst[0*dstStride+3]
+	dst[1*dstStride+3] = avg3(e, f, g)
+	dst[2*dstStride+2] = dst[1*dstStride+3]
+	dst[3*dstStride+1] = dst[1*dstStride+3]
+	dst[2*dstStride+3] = avg3(f, g, h)
+	dst[3*dstStride+2] = dst[2*dstStride+3]
+	dst[3*dstStride+3] = avg3(g, h, h)
+}
+
+func Intra4x4RDPredict(dst []byte, dstStride int, above []byte, left []byte, topLeft byte) {
+	_ = above[3]
+	_ = left[3]
+	_ = dst[3*dstStride+3]
+
+	a, b, c, d := above[0], above[1], above[2], above[3]
+	i, j, k, l := left[0], left[1], left[2], left[3]
+	x := topLeft
+
+	dst[3*dstStride+0] = avg3(j, k, l)
+	dst[3*dstStride+1] = avg3(i, j, k)
+	dst[2*dstStride+0] = dst[3*dstStride+1]
+	dst[3*dstStride+2] = avg3(x, i, j)
+	dst[2*dstStride+1] = dst[3*dstStride+2]
+	dst[1*dstStride+0] = dst[3*dstStride+2]
+	dst[3*dstStride+3] = avg3(a, x, i)
+	dst[2*dstStride+2] = dst[3*dstStride+3]
+	dst[1*dstStride+1] = dst[3*dstStride+3]
+	dst[0*dstStride+0] = dst[3*dstStride+3]
+	dst[2*dstStride+3] = avg3(b, a, x)
+	dst[1*dstStride+2] = dst[2*dstStride+3]
+	dst[0*dstStride+1] = dst[2*dstStride+3]
+	dst[1*dstStride+3] = avg3(c, b, a)
+	dst[0*dstStride+2] = dst[1*dstStride+3]
+	dst[0*dstStride+3] = avg3(d, c, b)
+}
+
+func Intra4x4VRPredict(dst []byte, dstStride int, above []byte, left []byte, topLeft byte) {
+	_ = above[3]
+	_ = left[2]
+	_ = dst[3*dstStride+3]
+
+	a, b, c, d := above[0], above[1], above[2], above[3]
+	i, j, k := left[0], left[1], left[2]
+	x := topLeft
+
+	dst[0*dstStride+0] = avg2(x, a)
+	dst[2*dstStride+1] = dst[0*dstStride+0]
+	dst[0*dstStride+1] = avg2(a, b)
+	dst[2*dstStride+2] = dst[0*dstStride+1]
+	dst[0*dstStride+2] = avg2(b, c)
+	dst[2*dstStride+3] = dst[0*dstStride+2]
+	dst[0*dstStride+3] = avg2(c, d)
+
+	dst[3*dstStride+0] = avg3(k, j, i)
+	dst[2*dstStride+0] = avg3(j, i, x)
+	dst[1*dstStride+0] = avg3(i, x, a)
+	dst[3*dstStride+1] = dst[1*dstStride+0]
+	dst[1*dstStride+1] = avg3(x, a, b)
+	dst[3*dstStride+2] = dst[1*dstStride+1]
+	dst[1*dstStride+2] = avg3(a, b, c)
+	dst[3*dstStride+3] = dst[1*dstStride+2]
+	dst[1*dstStride+3] = avg3(b, c, d)
+}
+
+func Intra4x4VLPredict(dst []byte, dstStride int, above []byte) {
+	_ = above[7]
+	_ = dst[3*dstStride+3]
+
+	a, b, c, d := above[0], above[1], above[2], above[3]
+	e, f, g, h := above[4], above[5], above[6], above[7]
+
+	dst[0*dstStride+0] = avg2(a, b)
+	dst[0*dstStride+1] = avg2(b, c)
+	dst[2*dstStride+0] = dst[0*dstStride+1]
+	dst[0*dstStride+2] = avg2(c, d)
+	dst[2*dstStride+1] = dst[0*dstStride+2]
+	dst[0*dstStride+3] = avg2(d, e)
+	dst[2*dstStride+2] = dst[0*dstStride+3]
+	dst[2*dstStride+3] = avg3(e, f, g)
+
+	dst[1*dstStride+0] = avg3(a, b, c)
+	dst[1*dstStride+1] = avg3(b, c, d)
+	dst[3*dstStride+0] = dst[1*dstStride+1]
+	dst[1*dstStride+2] = avg3(c, d, e)
+	dst[3*dstStride+1] = dst[1*dstStride+2]
+	dst[1*dstStride+3] = avg3(d, e, f)
+	dst[3*dstStride+2] = dst[1*dstStride+3]
+	dst[3*dstStride+3] = avg3(f, g, h)
+}
+
+func Intra4x4HDPredict(dst []byte, dstStride int, above []byte, left []byte, topLeft byte) {
+	_ = above[2]
+	_ = left[3]
+	_ = dst[3*dstStride+3]
+
+	a, b, c := above[0], above[1], above[2]
+	i, j, k, l := left[0], left[1], left[2], left[3]
+	x := topLeft
+
+	dst[0*dstStride+0] = avg2(i, x)
+	dst[1*dstStride+2] = dst[0*dstStride+0]
+	dst[1*dstStride+0] = avg2(j, i)
+	dst[2*dstStride+2] = dst[1*dstStride+0]
+	dst[2*dstStride+0] = avg2(k, j)
+	dst[3*dstStride+2] = dst[2*dstStride+0]
+	dst[3*dstStride+0] = avg2(l, k)
+
+	dst[0*dstStride+3] = avg3(a, b, c)
+	dst[0*dstStride+2] = avg3(x, a, b)
+	dst[0*dstStride+1] = avg3(i, x, a)
+	dst[1*dstStride+3] = dst[0*dstStride+1]
+	dst[1*dstStride+1] = avg3(j, i, x)
+	dst[2*dstStride+3] = dst[1*dstStride+1]
+	dst[2*dstStride+1] = avg3(k, j, i)
+	dst[3*dstStride+3] = dst[2*dstStride+1]
+	dst[3*dstStride+1] = avg3(l, k, j)
+}
+
+func Intra4x4HUPredict(dst []byte, dstStride int, left []byte) {
+	_ = left[3]
+	_ = dst[3*dstStride+3]
+
+	i, j, k, l := left[0], left[1], left[2], left[3]
+
+	dst[0*dstStride+0] = avg2(i, j)
+	dst[0*dstStride+2] = avg2(j, k)
+	dst[1*dstStride+0] = dst[0*dstStride+2]
+	dst[1*dstStride+2] = avg2(k, l)
+	dst[2*dstStride+0] = dst[1*dstStride+2]
+	dst[0*dstStride+1] = avg3(i, j, k)
+	dst[0*dstStride+3] = avg3(j, k, l)
+	dst[1*dstStride+1] = dst[0*dstStride+3]
+	dst[1*dstStride+3] = avg3(k, l, l)
+	dst[2*dstStride+1] = dst[1*dstStride+3]
+	dst[2*dstStride+3] = l
+	dst[2*dstStride+2] = l
+	dst[3*dstStride+0] = l
+	dst[3*dstStride+1] = l
+	dst[3*dstStride+2] = l
+	dst[3*dstStride+3] = l
+}
+
+func avg2(a byte, b byte) byte {
+	return byte((int(a) + int(b) + 1) >> 1)
+}
+
+func avg3(a byte, b byte, c byte) byte {
+	return byte((int(a) + 2*int(b) + int(c) + 2) >> 2)
+}
+
+func fillRow4(dst []byte, v byte) {
+	_ = dst[3]
+	dst[0] = v
+	dst[1] = v
+	dst[2] = v
+	dst[3] = v
+}
---
+++ internal/vp8/dsp/intra4x4_test.go
@@ -0,0 +1,134 @@
+package dsp
+
+import (
+	"testing"
+
+	"github.com/thesyncim/libgopx/internal/vp8/common"
+)
+
+func TestIntra4x4PredictModes(t *testing.T) {
+	above := []byte{10, 20, 30, 40, 50, 60, 70, 80}
+	left := []byte{90, 100, 110, 120}
+	const topLeft = 5
+
+	cases := []struct {
+		name string
+		mode common.BPredictionMode
+		want [16]byte
+	}{
+		{name: "dc", mode: common.BDCPred, want: [16]byte{
+			65, 65, 65, 65,
+			65, 65, 65, 65,
+			65, 65, 65, 65,
+			65, 65, 65, 65,
+		}},
+		{name: "tm", mode: common.BTMPred, want: [16]byte{
+			95, 105, 115, 125,
+			105, 115, 125, 135,
+			115, 125, 135, 145,
+			125, 135, 145, 155,
+		}},
+		{name: "ve", mode: common.BVEPred, want: [16]byte{
+			11, 20, 30, 40,
+			11, 20, 30, 40,
+			11, 20, 30, 40,
+			11, 20, 30, 40,
+		}},
+		{name: "he", mode: common.BHEPred, want: [16]byte{
+			71, 71, 71, 71,
+			100, 100, 100, 100,
+			110, 110, 110, 110,
+			118, 118, 118, 118,
+		}},
+		{name: "ld", mode: common.BLDPred, want: [16]byte{
+			20, 30, 40, 50,
+			30, 40, 50, 60,
+			40, 50, 60, 70,
+			50, 60, 70, 78,
+		}},
+		{name: "rd", mode: common.BRDPred, want: [16]byte{
+			28, 11, 20, 30,
+			71, 28, 11, 20,
+			100, 71, 28, 11,
+			110, 100, 71, 28,
+		}},
+		{name: "vr", mode: common.BVRPred, want: [16]byte{
+			8, 15, 25, 35,
+			28, 11, 20, 30,
+			71, 8, 15, 25,
+			100, 28, 11, 20,
+		}},
+		{name: "vl", mode: common.BVLPred, want: [16]byte{
+			15, 25, 35, 45,
+			20, 30, 40, 50,
+			25, 35, 45, 60,
+			30, 40, 50, 70,
+		}},
+		{name: "hd", mode: common.BHDPred, want: [16]byte{
+			48, 28, 11, 20,
+			95, 71, 48, 28,
+			105, 100, 95, 71,
+			115, 110, 105, 100,
+		}},
+		{name: "hu", mode: common.BHUPred, want: [16]byte{
+			95, 100, 105, 110,
+			105, 110, 115, 118,
+			115, 118, 120, 120,
+			120, 120, 120, 120,
+		}},
+	}
+
+	for _, tc := range cases {
+		dst := make([]byte, 4*8)
+		for i := range dst {
+			dst[i] = 222
+		}
+
+		if ok := Intra4x4Predict(dst, 8, tc.mode, above, left, topLeft); !ok {
+			t.Fatalf("%s returned false", tc.name)
+		}
+
+		for y := 0; y < 4; y++ {
+			for x := 0; x < 4; x++ {
+				want := tc.want[y*4+x]
+				if got := dst[y*8+x]; got != want {
+					t.Fatalf("%s dst[%d,%d] = %d, want %d", tc.name, x, y, got, want)
+				}
+			}
+			for x := 4; x < 8; x++ {
+				if got := dst[y*8+x]; got != 222 {
+					t.Fatalf("%s dst[%d,%d] = %d, want sentinel", tc.name, x, y, got)
+				}
+			}
+		}
+	}
+}
+
+func TestIntra4x4PredictInvalidMode(t *testing.T) {
+	dst := make([]byte, 4*4)
+	if ok := Intra4x4Predict(dst, 4, common.Above4x4, nil, nil, 0); ok {
+		t.Fatalf("invalid intra mode returned true")
+	}
+}
+
+func TestIntra4x4PredictAllocatesZero(t *testing.T) {
+	above := []byte{10, 20, 30, 40, 50, 60, 70, 80}
+	left := []byte{90, 100, 110, 120}
+	dst := make([]byte, 4*4)
+	allocs := testing.AllocsPerRun(1000, func() {
+		Intra4x4Predict(dst, 4, common.BRDPred, above, left, 5)
+	})
+	if allocs != 0 {
+		t.Fatalf("allocs = %v, want 0", allocs)
+	}
+}
+
+func BenchmarkIntra4x4Predict(b *testing.B) {
+	above := []byte{10, 20, 30, 40, 50, 60, 70, 80}
+	left := []byte{90, 100, 110, 120}
+	dst := make([]byte, 4*4)
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		Intra4x4Predict(dst, 4, common.BRDPred, above, left, 5)
+	}
+}
