e3b67a83c745bb6c356a4f69f8708eed26a40b98
thesyncim <thesyncim@gmail.com>
2026-05-14 10:17:33 +0100

    vp8: cover deadline runtime resize parity

--- oracle_encoder_stream_parity_resize_test.go
+++ oracle_encoder_stream_parity_resize_test.go
@@ -354,6 +354,8 @@ func TestOracleEncoderStreamByteParityRuntimeResizeFrameFlags(t *testing.T) {
 		{name: "64x64-to-32x32-realtime-cpu-3-vbr", w1: 64, h1: 64, w2: 32, h2: 32, deadline: DeadlineRealtime, cpuUsed: -3, rcMode: RateControlVBR},
 		{name: "64x64-to-32x32-realtime-cpu-3-cq20", w1: 64, h1: 64, w2: 32, h2: 32, deadline: DeadlineRealtime, cpuUsed: -3, rcMode: RateControlCQ, cqLevel: 20},
 		{name: "64x64-to-32x32-realtime-cpu-3-q20", w1: 64, h1: 64, w2: 32, h2: 32, deadline: DeadlineRealtime, cpuUsed: -3, rcMode: RateControlQ, cqLevel: 20},
+		{name: "64x64-to-32x32-good-cpu0-cbr", w1: 64, h1: 64, w2: 32, h2: 32, deadline: DeadlineGoodQuality, cpuUsed: 0, rcMode: RateControlCBR},
+		{name: "64x64-to-32x32-best-cpu0-cbr", w1: 64, h1: 64, w2: 32, h2: 32, deadline: DeadlineBestQuality, cpuUsed: 0, rcMode: RateControlCBR},
 		{name: "65x33-to-33x17-realtime-cpu0-cbr", w1: 65, h1: 33, w2: 33, h2: 17, deadline: DeadlineRealtime, cpuUsed: 0, rcMode: RateControlCBR},
 		{name: "96x96-to-64x64-good-cpu0-vbr", w1: 96, h1: 96, w2: 64, h2: 64, deadline: DeadlineGoodQuality, cpuUsed: 0, rcMode: RateControlVBR},
 	}
